82d55193430975472f490217d11b03ad191f6704
Yuqi Huai <yhuai@uci.edu>
2023-06-13 02:47:05 -0700

    update(scripts): use cli argument and allow run only jars

--- Darcy/runJars.sh
+++ Darcy/runJars.sh
@@ -1,60 +1,8 @@
-#!/bin/bash
-
-# check if argument is provided
-if [ -z "$1" ]
-  then
-    >&2 echo "No project path argument supplied"
-    exit 1
-fi
-
-projectPath=$1
-# check if argument is an existing directory
-if [ ! -d "$projectPath" ]
-  then
-    >&2 echo "Project path argument is not a directory"
-    exit 1
-fi
-
 JAVA_8_HOME=/usr/lib/jvm/amazon-corretto-8.372.07.1-linux-x64
 JAVA_17_HOME=/usr/lib/jvm/jdk-17
 
-foo=" "
-rm -r ./XMLreports/
-mkdir XMLreports
-
-echo "======== Constructing modules.txt and module-infos.txt ========"
 start_pre=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
 start_glob=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-for f in $(find $projectPath -type f -name 'module-info.class')
-do
-    DIR=$(dirname "${f}")
-    foo="$foo${DIR}#"
-done
-echo $foo > ./modules.txt
-
-for f in $(find $projectPath -type f -name 'module-info.java')
-do
-    DIR=$(dirname "${f}")
-    dirpath="$dirpath${DIR}#"    
-done
-echo $dirpath > ./module-infos.txt
-end_pre=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-elapsed_pre=$((end_pre - start_pre))
-
-echo "======== Static Analysis Using Classycle ========"
-start_classycle=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-python ./TraverseDir.py $projectPath
-end_classycle=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-elapsed_classycle=$((end_classycle - start_classycle))
-echo "-------- Classycle Static Analysis Finished --------"
-
-echo "======== Parsing XML Files ========"
-start_parse=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-python ./ParseXML.py
-end_parse=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
-elapsed_parse=$((end_parse - start_parse))
-echo "-------- Finished Parsing XML Files --------"
-
 echo "======== Checking for Inconsistencies ========"
 start_incon=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
 java -jar Check_Consistency.jar
@@ -100,8 +48,6 @@ echo "======== Stats ========"
 end_glob=$(ruby -e 'puts (Time.now.to_f * 1000).to_i')
 elapsed_total=$((end_glob - start_glob))
 echo "pre-process: $elapsed_pre ms"
-echo "Static Analysis (Classycle): $elapsed_classycle ms"
-echo "Parsing XML: $elapsed_parse ms"
 echo "Checking Inconsistencies: $elapsed_incon ms"
 echo "Reflection Analysis: $elapsed_open ms"
 echo "Uses Analysis: $elapsed_uses ms"
